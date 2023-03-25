0f838d62722feca7c5c3729a588afbb31f3aa71e
Fe1ker <93716685+Ma1heusSantos@users.noreply.github.com>
2023-03-25 17:53:36 -0300

    criando componentes

--- projeto/src/app/app.module.ts
+++ projeto/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AplicacaoComponent } from './components/aplicacao/aplicacao.component';
+import { ConceitosComponent } from './components/conceitos/conceitos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AplicacaoComponent,
+    ConceitosComponent
   ],
   imports: [
     BrowserModule,
---
+++ projeto/src/app/components/conceitos/conceitos.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-conceitos',
+  templateUrl: './conceitos.component.html',
+  styleUrls: ['./conceitos.component.css']
+})
+export class ConceitosComponent {
+
+}
